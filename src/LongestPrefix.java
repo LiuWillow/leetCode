ed4eef14b2a44c9b7def5c6ff027c30a13213298
LiuWillow <liuwillow9625@163.com>
2019-04-02 17:54:32 +0800

    三数之和

--- src/LongestPrefix.java
+++ src/LongestPrefix.java
@@ -2,6 +2,14 @@
  * @author lwl
  * @date 2019/3/28 17:25
  * @description
+ * 14.编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1:
+ *
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
  */
 public class LongestPrefix {
     public String longestCommonPrefix(String[] strs) {
@@ -10,6 +18,7 @@ public class LongestPrefix {
         }
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
+            //str.indexOf(str2)返回0表示匹配前缀，-1表示不匹配
             while (strs[i].indexOf(prefix) != 0){
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.isEmpty()){
