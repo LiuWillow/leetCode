362d6490f195fd258f67db5e7e0eec5efbd2aaba
LiuWillow <liuwillow9625@163.com>
2019-05-06 18:04:04 +0800

    完成合并两个有序链表

---
+++ src/main/java/Q21_MergeTwoLists.java
@@ -0,0 +1,71 @@
+/**
+ * @author liuweilong
+ * @description
+ * @date 2019/5/6 17:41
+ */
+public class Q21_MergeTwoLists {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        ListNode add(int i) {
+            ListNode next = new ListNode(i);
+            this.next = next;
+            return next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode result = null;
+        ListNode next = null;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                int tv2 = l2.val;
+                l2 = l2.next;
+                if (result == null){
+                    result = new ListNode(tv2);
+                    next = result;
+                    continue;
+                }
+                next.next = new ListNode(tv2);
+                next = next.next;
+                continue;
+            }
+            if (l2 == null) {
+                int tv1 = l1.val;
+                l1 = l1.next;
+                if (result == null){
+                    result = new ListNode(tv1);
+                    next = result;
+                    continue;
+                }
+                next.next = new ListNode(tv1);
+                next = next.next;
+                continue;
+            }
+            int v1 = l1.val;
+            int v2 = l2.val;
+            int v;
+            if (v1 < v2){
+                v = l1.val;
+                l1 = l1.next;
+            }else {
+                v = l2.val;
+                l2 = l2.next;
+            }
+
+            if (result == null){
+                result = new ListNode(v);
+                next = result;
+                continue;
+            }
+            next.next = new ListNode(v);
+            next = next.next;
+        }
+        return result;
+    }
+}
