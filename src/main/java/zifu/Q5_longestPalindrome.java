40aba2fc3882950d95d5625da549d50d01d49b2f
liuwillow <liuwillow9625@163.com>
2020-01-15 17:22:15 +0800

    最长回文

--- src/main/java/zifu/Q5_longestPalindrome.java
+++ src/main/java/zifu/Q5_longestPalindrome.java
@@ -3,7 +3,20 @@ package zifu;
 /**
  * author liuweilong
  * date 2019/8/29 10:31
- * desc 最长回文
+ * desc
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ * babada
  */
 public class Q5_longestPalindrome {
     public String longestPalindrome(String s) {
