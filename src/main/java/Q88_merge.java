5b00fe0b9b24d1bfb113ee19407098ef0e375e08
liuweilong <1274218135@qq.com>
2021-05-11 18:39:48 +0800

    合并数组

---
+++ src/main/java/Q88_merge.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+/**
+ * @author liuweilong
+ * @date 2021/5/11
+ * @description 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ * <p>
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+ * 输出：[1]
+ */
+public class Q88_merge {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int tail = m + n - 1;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
+                nums1[tail--] = nums2[p2--];
+            } else if (p2 == -1) {
+                nums1[tail--] = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                nums1[tail--] = nums1[p1--];
+            } else {
+                nums1[tail--] = nums2[p2--];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{4, 8, 11, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = new int[]{2, 9, 10};
+        int n = 3;
+        new Q88_merge().merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
