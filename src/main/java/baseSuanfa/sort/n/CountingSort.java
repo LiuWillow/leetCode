f4c50103a2ff4fa9bc60fe83beeeb8cecaf5b602
liuweilong <liuwillow9625@163.com>
2021-10-21 11:49:43 +0800

    整理基础排序

--- src/main/java/baseSuanfa/sort/n/CountingSort.java
+++ src/main/java/baseSuanfa/sort/n/CountingSort.java
@@ -5,47 +5,44 @@ import java.util.Arrays;
 /**
  * author liuweilong
  * date 2019/11/2 18:04
- * desc 计数排序
+ * desc 计数排序，适用于目标在某一个数字范围内，将数字范围作为计数数组的index，记录每个数字出现的次数，然后将计数数组翻译成排序后的数组
  */
 public class CountingSort {
-    public int[] sort(int[] nums) {
-        //先遍历一遍，拿到最大值和最小值
+    public int[] test(int[] nums) {
+        // 先找到最大值，确定计数数组长度 max+1
+        if (nums == null || nums.length == 0) {
+            return nums;
+        }
+
         int max = nums[0];
-        int min = max;
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(nums[i], max);
+        }
+
+        // 创建计数数组，遍历nums，计数
+        int[] countArray = new int[max + 1];
         for (int num : nums) {
-            if (num > max) {
-                max = num;
-            }
-            if (num < min) {
-                min = num;
-            }
+            countArray[num]++;
         }
-        //根据最大值和最小值分桶
-        int[] buckets = new int[max - min + 1];
-        //遍历数组，将得分人数统计在每个桶中
-        for (int num : nums) {
-            buckets[num - min]++;
-        }
-        //累加桶数据
-        for (int i = 1; i < buckets.length; i++) {
-            buckets[i] = buckets[i] + buckets[i - 1];
-        }
-        //再次遍历数组，获取对应下标，对应数字减一
+
         int[] result = new int[nums.length];
-        for (int num : nums) {
-            int rank = buckets[num - min];
-            if (rank > 0) {
-                result[--buckets[num - min]] = num;
-                continue;
+        //创建新数组，根据计数数组把结果赋值给新数组
+        int j = 0;
+        for (int i = 0; i < countArray.length; i++) {
+            int count = countArray[i];
+            if (count != 0) {
+                for (int k = 0; k < count; k++) {
+                    result[j++] = i;
+                }
             }
-            result[rank] = num;
         }
+
         return result;
     }
 
     public static void main(String[] args) {
         int[] nums = new int[]{2, 3, 4, 6, 3, 4, 2, 3, 3, 4, 1, 1, 1};
-        int[] result = new CountingSort().sort(nums);
+        int[] result = new CountingSort().test(nums);
         System.out.println(Arrays.toString(result));
     }
 }
